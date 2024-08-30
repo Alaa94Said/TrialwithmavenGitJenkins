91eb6a055add48d14855ea3f7cb947f7e284c11a
Alaa Sayed <alaa.abdellal@gmail.com>
2024-08-30 23:24:51 +0300

    scheduled change jenkins

--- src/test/java/Driver/Selenium1/Chrome.java
+++ src/test/java/Driver/Selenium1/Chrome.java
@@ -10,14 +10,14 @@ public class Chrome {
 	
 	@ Test
 	public static void browserOpen() {
-		
 	
 		System.setProperty("webdriver.chrome.driver","//Users//lillys//Desktop//Lilly//setup//chromedriver");
 		ChromeDriver driver = new ChromeDriver() ; 
 		driver.navigate().to("http://www.google.com");
 		String title = driver.getTitle();
 		System.out.println("title is same");
-		driver.quit();
+		System.out.println("title is lilly");
+		driver.close();
 
 	}
 
